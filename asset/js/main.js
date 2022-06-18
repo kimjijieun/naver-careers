1caa810c6d65a8c41bd83e043982347036eae996
kimjijieun <kim217304@naver.com>
2022-06-18 12:05:48 +0900

    수정2

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,57 @@
 $(function(){
 
-    var mainslide = new Swiper(".mainslide", {
+    // 닫기 사라지기
+    $('.sc-notice .close').click(function () {
+        $('.sc-notice').hide();
+    })
+
+    // 메뉴호버
+    $('.menu-area .show').hover(function(){
+        $('.hide-menu').stop().slideDown();
+    }, function(){
+        $('.hide-menu').stop().slideUp();
+    })
+    
+    // $('.menu-area .show').hover(function(){
+    //     $('hide-menu').addClass('active')
+    // })
+
+    // 버튼 클릭시 관련사이트 나오기
+    $('.tab-area .button').click(function(){
+        $('.tab-list').stop().slideUp();
+    }, function(){
+        $('.tab-list').stop().slideDown();
+    })
+    
+    
+    // $('.tab-area .button').click(function(e){
+    //     e.preventDefault();
+    //     $('.gnb').addClass('on');
+    // });
+    // $('.tab-area .button').click(function(e){
+    //     e.preventDefault();
+    //     $('.gnb').removeClass('on');
+    // });
+
+    
+
+
+    // 메인슬라이드
+        var mainslide = new Swiper(".mainslide", {
+        });
+
+
+
+// 두번째 슬라이드
+    var midslide = new Swiper(".midslide", {
+        slidesPerView: "1.5",
+        spaceBetween: 30,
     });
-
-
+//세번째 슬라이드
+    var lastslide = new Swiper(".lastslide", {
+        slidesPerView: "3",
+        spaceBetween: 30,
+    });
 
 
 });
